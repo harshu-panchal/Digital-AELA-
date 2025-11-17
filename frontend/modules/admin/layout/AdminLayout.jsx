a0a7b0a29ce52af501b4e86b1eb1a4b925edd901
harshu-panchal <harshvardhanpanc145@gmail.com>
2025-11-17 23:53:50 +0530

    live room moderation6

--- frontend/modules/admin/layout/AdminLayout.jsx
+++ frontend/modules/admin/layout/AdminLayout.jsx
@@ -7,7 +7,7 @@ const AdminLayout = () => {
       <div className="flex">
         <AdminSidebar />
         <main className="ml-64 flex-1">
-          <div className="pt-[100px] pl-[40px]">
+          <div className="pt-[110px] pl-[30px] pb-[20px] pr-[30px]">
             <Outlet />
           </div>
         </main>
